32006a004aa13e16f1e69ea7c67671df52a1d873
monicaroy <monica_roy@brown.edu>
2020-12-06 14:42:35 -0500

    add global data

--- shapes/drawer.cpp
+++ shapes/drawer.cpp
@@ -7,6 +7,11 @@ using namespace CS123::GL;
 Drawer::Drawer() {
     loadPhongShader();
     createLights();
+
+    // setting up global data
+    m_global_data.ka = 0.5;
+    m_global_data.kd = 0.5;
+    m_global_data.ks = 0.5;
 }
 
 Drawer::~Drawer() {
